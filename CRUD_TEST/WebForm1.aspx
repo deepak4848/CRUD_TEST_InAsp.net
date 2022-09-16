75959108bae0a6364c2df7401f31641e043a2e2e
deepak4848 <111887778+deepak4848@users.noreply.github.com>
2022-09-16 18:32:54 +0530

    Add project files.

---
+++ CRUD_TEST/WebForm1.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CRUD_TEST.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name :</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Age :</td>
+                    <td><asp:TextBox ID="txtage" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Salary :</td>
+                    <td><asp:TextBox ID="txtsalary" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="empgv" runat="server" AutoGenerateColumns="false" OnRowCommand="empgv_RowCommand">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Employee Id">
+                                <ItemTemplate>
+                                    <%#Eval("id") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Employee Name">
+                                <ItemTemplate>
+                                    <%#Eval("name") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Employee age">
+                                <ItemTemplate>
+                                    <%#Eval("age") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Employee Salary">
+                                <ItemTemplate>
+                                    <%#Eval("salary") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:Button ID="btndelete" runat="server" CommandName="A" Text="Delete" CommandArgument='<%#Eval("id") %>' />
+                                    <asp:Button ID="btnedit" runat="server" CommandName="B" Text="Edit" CommandArgument='<%#Eval("id") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CRUD_TEST/WebForm1.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CRUD_TEST
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ABC"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Dispaly();
+            }
+        }
+        public void Clear()
+        {
+            txtname.Text = "";
+            txtage.Text = "";
+            txtsalary.Text = "";
+        }
+
+        public void Dispaly()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from employee", con);
+            SqlDataAdapter dd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dd.Fill(dt);
+            con.Close();
+            empgv.DataSource = dt;
+            empgv.DataBind();
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into employee(name,age,salary)values('" + txtname.Text + "','" + txtage.Text + "','" + txtsalary.Text + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Dispaly();
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update employee set name='" + txtname.Text + "', age='" + txtage.Text + "', salary='" + txtsalary.Text + "' where id='"+ ViewState["IDD"] + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                btnsave.Text = "Save";
+                Dispaly();
+            }
+            Clear();
+        }
+
+        protected void empgv_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from employee where id='" + e.CommandArgument + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Dispaly();
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from employee where id='"+e.CommandArgument+"'", con);
+                SqlDataAdapter dd = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                dd.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0]["name"].ToString();
+                txtage.Text = dt.Rows[0]["age"].ToString();
+                txtsalary.Text = dt.Rows[0]["salary"].ToString();
+                ViewState["IDD"] = e.CommandArgument;
+                btnsave.Text = "Update";
+            }
+        }
+    }
+}
